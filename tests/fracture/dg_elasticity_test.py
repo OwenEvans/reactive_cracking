2319fe619b47835019bfe9be1342b7455e3d245a
Owen Evans <oevans44@gmail.com>
2015-08-19 08:56:26 -0400

    introducing damage parameter (Versino et al. 2015). No damage evolution yet.

--- tests/fracture/dg_elasticity_test.py
+++ tests/fracture/dg_elasticity_test.py
@@ -1,6 +1,5 @@
 from dolfin import *
 
-# FIXME: Make mesh ghosted
 parameters["ghost_mode"] = "shared_facet"
 
 # Define class marking Dirichlet boundary (x = 0 or x = 1)
@@ -18,12 +17,13 @@ class BottomDirichletBoundary(SubDomain):
     return on_boundary and near(x[1], 0)
 
 # Create mesh and define function space
-mesh = UnitSquareMesh(24, 24)
+mesh = RectangleMesh(0.0, 0.0, 1.0, 1.0, 50, 2)
 V = VectorFunctionSpace(mesh, 'DG', 2)
 
 # Define test and trial functions
-u = TrialFunction(V)
+du = TrialFunction(V)
 v = TestFunction(V)
+u = Function(V)
 
 # Define normal vector and mesh size
 n = FacetNormal(mesh)
@@ -32,7 +32,7 @@ h_avg = (h('+') + h('-'))/2
 
 # Define the source term f, Dirichlet term u0 and Neumann term g
 f = Expression(('0.0','0.0'))
-u0 = Expression(('0.0', '10.0'))
+u0 = Expression(('0.0', '5.0'))
 g = Expression(('0.0', '0.0'))
 u1 = Expression(('0.0', '0.0'))
 
@@ -46,29 +46,31 @@ BottomDirichletBoundary().mark(boundaries, 3)
 ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
 
 # Define parameters
-alpha = 4.0
-gamma = 8.0
+alpha = 10.0
+gamma = 5.0
 
+# Stress computation
 def sigma(v):
   return sym(grad(v))
 
 # Define variational problem
-a = inner(sigma(u), grad(v))*dx \
-   - inner(jump(u), avg(sigma(v))*n('+'))*dS \
-   - inner(avg(sigma(u))*n('+'), jump(v))*dS \
-   + alpha/h_avg*inner(jump(u), jump(v))*dS \
-   - inner(u, sigma(v)*n)*ds(1) \
+t_star = avg(sigma(u))*n('+') - (alpha/h_avg)*jump(u)
+D = Constant(0.5)
+
+r = inner(sigma(u), grad(v))*dx \
+   - (1 - D)*inner(t_star, jump(v))*dS \
    - dot(sigma(u)*n, v)*ds(1) \
    + (gamma/h)*inner(u, v)*ds(1) \
-   - inner(u, sigma(v)*n)*ds(3) \
    - dot(sigma(u)*n, v)*ds(3) \
-   + (gamma/h)*inner(u, v)*ds(3)
+   + (gamma/h)*inner(u, v)*ds(3) \
+   - D*inner(jump(v), n('+'))*0.5*(abs(dot(-t_star, n('+'))) - dot(t_star, n('+')))*dS \
+   - inner(v, f)*dx - inner(g, v)*ds(2) - (gamma/h)*inner(u0, v)*ds(1) - (gamma/h)*inner(u1, v)*ds(3) 
 
-L = inner(v, f)*dx + inner(g, v)*ds(2) - inner(u0, sigma(v)*n)*ds(1) + (gamma/h)*inner(u0, v)*ds(1) - inner(u1, sigma(v)*n)*ds(3) + (gamma/h)*inner(u1, v)*ds(3) 
+# Compute jacobian
+J = derivative(r, u, du)
 
-# Compute solution
-u = Function(V)
-solve(a == L, u)
+# Solve
+solve(r == 0, u, J=J, solver_parameters={"newton_solver": {'absolute_tolerance': 1E-8, 'relative_tolerance': 1E-8, 'maximum_iterations': 25, 'relaxation_parameter': 1.0}})
 print("Solution vector norm (0): {!r}".format(u.vector().norm("l2")))
 
 # Save solution to file
